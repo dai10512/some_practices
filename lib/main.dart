1b35cc33be1ae87402683b05ecd958d7ed74d30f
Daisuke Osanai <dsk.osanai@gmail.com>
2023-01-02 23:43:08 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,92 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+
+import 'main_model.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
+      home: PixabayPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class PixabayPage extends StatefulWidget {
+  const PixabayPage({Key? key}) : super(key: key);
+
+  @override
+  State<PixabayPage> createState() => _PixabayPageState();
+}
+
+class _PixabayPageState extends State<PixabayPage> {
+  List imageList = [];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: TextFormField(
+          decoration: const InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+          ),
+          onFieldSubmitted: (text) async {
+            print(text);
+            imageList = await fetchImages(text);
+            setState(() {});
+          },
+        ),
+      ),
+      body: GridView.builder(
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemCount: imageList.length,
+        itemBuilder: (context, index) {
+          final image = imageList[index];
+          return InkWell(
+            onTap: () async {
+              onTap(image['webformatURL']);
+              // print(image['likes']);
+              // Directory dir = await getTemporaryDirectory();
+              // Response response = await Dio().get(
+              //   image['webformatURL'],
+              //   options: Options(
+              //     responseType: ResponseType.bytes,
+              //   ),
+              // );
+              // final imageFile = await File('${dir.path}/image.png')
+              //     .writeAsBytes(response.data);
+              // await Share.shareXFiles([XFile(imageFile.path)]);
+            },
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Image.network(
+                  image['previewURL'],
+                  fit: BoxFit.cover,
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                      color: Colors.white,
+                      child: Text(' 👍${image['likes'].toString()} ')),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
---
+++ lib/main_model.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+
+Future<List> fetchImages(String text) async {
+  const pixabayKey = '32493323-0db8d050baa6e6cc76d89fb4b';
+  const perPage = 100;
+
+  var options = BaseOptions(
+    baseUrl: 'https://pixabay.com/api',
+    connectTimeout: 5000,
+    receiveTimeout: 3000,
+  );
+  final dio = Dio(options);
+
+  Response response = await dio.get(
+    '',
+    queryParameters: {
+      'key': pixabayKey,
+      'q': text,
+      'image_type': 'photo',
+      'per_page': perPage,
+    },
+  );
+  print(response.data);
+
+  final imageList = response.data['hits'];
+  return imageList;
+}
+
+Future<void> onTap(String imagePath) async {
+  Directory dir = await getTemporaryDirectory();
+  Response response = await Dio().get(
+    imagePath,
+    options: Options(
+      responseType: ResponseType.bytes,
+    ),
+  );
+  final imageFile =
+      await File('${dir.path}/image.png').writeAsBytes(response.data);
+  await Share.shareXFiles([XFile(imageFile.path)]);
+}
