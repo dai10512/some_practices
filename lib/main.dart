4ca34a6b9e4657095d6f374c2d80923107105366
Daisuke Osanai <dsk.osanai@gmail.com>
2023-01-30 00:04:16 +0900

    fix logi

--- lib/main.dart
+++ lib/main.dart
@@ -16,24 +16,22 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: PixabayPage(),
+      // home: HomePage(),
     );
   }
 }
 
-class PixabayPage extends StatefulWidget {
-  const PixabayPage({Key? key}) : super(key: key);
+class PixabayPage extends ConsumerWidget {
+  PixabayPage({Key? key}) : super(key: key);
+
+  final textController = TextEditingController();
 
   @override
-  State<PixabayPage> createState() => _PixabayPageState();
-}
+  Widget build(context, ref) {
+    final asyncValue = ref.watch(futureProvider(textController.text));
 
-class _PixabayPageState extends State<PixabayPage> {
-  List imageList = [];
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -44,11 +42,11 @@ class _PixabayPageState extends State<PixabayPage> {
                 fillColor: Colors.white,
                 filled: true,
               ),
+              controller: textController,
               onFieldSubmitted: (text) async {
-                print(text);
-                ref.refresh(imagesProvider('flower'));
-                // imageList = await ref.read(imagesProvider(text));
-                setState(() {});
+                print('submitt!!!!!!!!!!!!!!!');
+                ref.refresh(futureProvider(text));
+                ref.refresh(futureProvider(textController.text));
               },
             );
           },
@@ -56,24 +54,32 @@ class _PixabayPageState extends State<PixabayPage> {
       ),
       body: Consumer(
         builder: (context, ref, _) {
-          final asyncValue = ref.watch(imagesProvider('sea'));
           return asyncValue.when(
             error: (error, stackTrace) => const Text('error'),
-            data: (data) => const Text('data'),
-            loading: () => _gridView(),
+            loading: () => const Text('loading'),
+            data: (data) => _gridView(data),
           );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.refresh),
+        onPressed: () async {
+          // 状態を更新する
+          print('click!!!!!!!!!!!!!!!!!!!!!!!!');
+          ref.read(futureProvider(textController.text));
+          ref.refresh(futureProvider(textController.text));
         },
       ),
     );
   }
 
-  Widget _gridView() {
+  Widget _gridView(List data) {
     return GridView.builder(
       gridDelegate:
           const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-      itemCount: imageList.length,
+      itemCount: data.length,
       itemBuilder: (context, index) {
-        final image = imageList[index];
+        final image = data[index];
         final pixabayImage = PixabayImage.fromMap(image);
         return InkWell(
           onTap: () async {
@@ -96,6 +102,40 @@ class _PixabayPageState extends State<PixabayPage> {
           ),
         );
       },
+    );
+  }
+}
+
+// FutureProviderの作成 (単一のデータを非同期で取得する)
+final futureProvider1 = FutureProvider<dynamic>((ref) async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 'Hello World';
+});
+
+// FutureProviderを作成すると「AsyncValue」オブジェクトを生成できる
+//「AsyncValue」は非同期通信の通信中、通信終了、異常終了処理をハンドリングしてくれるRiverpodの便利な機能のこと
+
+class HomePage extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // AsyncValueオブジェクトを取得する
+    final asyncValue = ref.watch(futureProvider1);
+    return Scaffold(
+      appBar: AppBar(title: const Text('Hallo World')),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.refresh),
+        onPressed: () {
+          // 状態を更新する
+          ref.refresh(futureProvider1);
+        },
+      ),
+      body: Center(
+        child: asyncValue.when(
+          error: (err, _) => Text(err.toString()), //エラー時
+          loading: () => const CircularProgressIndicator(), //読み込み時
+          data: (data) => Text(data.toString()), //データ受け取り時
+        ),
+      ),
     );
   }
 }
--- lib/main_model.dart
+++ lib/main_model.dart
@@ -5,64 +5,32 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
-final futureProvider = FutureProvider<dynamic>((ref) async {
-  await Future.delayed(const Duration(seconds: 3));
-  return 'Hello World';
+final futureProvider = FutureProvider.family((ref, text) async {
+  const pixabayKey = '32493323-0db8d050baa6e6cc76d89fb4b';
+  const perPage = 100;
+
+  var options = BaseOptions(
+    baseUrl: 'https://pixabay.com/api',
+    connectTimeout: 5000,
+    receiveTimeout: 3000,
+  );
+  final dio = Dio(options);
+
+  Response response = await dio.get(
+    '',
+    queryParameters: {
+      'key': pixabayKey,
+      'q': text,
+      'image_type': 'photo',
+      'per_page': perPage,
+    },
+  );
+  print(response.data);
+
+  final imageList = response.data['hits'];
+  return imageList;
 });
 
-final imagesProvider = FutureProvider.family<WidgetRef, String>(
-  ((ref, text) async {
-    const pixabayKey = '32493323-0db8d050baa6e6cc76d89fb4b';
-    const perPage = 100;
-
-    var options = BaseOptions(
-      baseUrl: 'https://pixabay.com/api',
-      connectTimeout: 5000,
-      receiveTimeout: 3000,
-    );
-    final dio = Dio(options);
-
-    Response response = await dio.get(
-      '',
-      queryParameters: {
-        'key': pixabayKey,
-        'q': text,
-        'image_type': 'photo',
-        'per_page': perPage,
-      },
-    );
-    print(response.data);
-
-    final imageList = response.data['hits'];
-    return imageList;
-  }),
-);
-
-// Future<List> fetchImages(String text) async {
-//   const pixabayKey = '32493323-0db8d050baa6e6cc76d89fb4b';
-//   const perPage = 100;
-
-//   var options = BaseOptions(
-//     baseUrl: 'https://pixabay.com/api',
-//     connectTimeout: 5000,
-//     receiveTimeout: 3000,
-//   );
-//   final dio = Dio(options);
-
-//   Response response = await dio.get(
-//     '',
-//     queryParameters: {
-//       'key': pixabayKey,
-//       'q': text,
-//       'image_type': 'photo',
-//       'per_page': perPage,
-//     },
-//   );
-//   print(response.data);
-
-//   final imageList = response.data['hits'];
-//   return imageList;
-// }
 
 Future<void> shareImage(String imagePath) async {
   Directory dir = await getTemporaryDirectory();
