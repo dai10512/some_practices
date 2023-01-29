3889e2e4e05581ad0b6e1d0af2fe639e155701a4
Daisuke Osanai <dsk.osanai@gmail.com>
2023-01-29 21:44:54 +0900

    set another version

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'main_model.dart';
 import 'pixabay_image.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -32,48 +37,65 @@ class _PixabayPageState extends State<PixabayPage> {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        title: TextFormField(
-          decoration: const InputDecoration(
-            fillColor: Colors.white,
-            filled: true,
-          ),
-          onFieldSubmitted: (text) async {
-            print(text);
-            imageList = await fetchImages(text);
-            setState(() {});
+        title: Consumer(
+          builder: (context, ref, _) {
+            return TextFormField(
+              decoration: const InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+              ),
+              onFieldSubmitted: (text) async {
+                print(text);
+                ref.refresh(imagesProvider('flower'));
+                // imageList = await ref.read(imagesProvider(text));
+                setState(() {});
+              },
+            );
           },
         ),
       ),
-      body: GridView.builder(
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-        itemCount: imageList.length,
-        itemBuilder: (context, index) {
-          final image = imageList[index];
-          final pixabayImage = PixabayImage.fromMap(image);
-          return InkWell(
-            onTap: () async {
-              shareImage(pixabayImage.webformatURL.toString());
-            },
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                Image.network(
-                  pixabayImage.previewURL.toString(),
-                  fit: BoxFit.cover,
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Container(
-                      color: Colors.white,
-                      child: Text(
-                          ' 👍${pixabayImage.likes.toString()} ')),
-                ),
-              ],
-            ),
+      body: Consumer(
+        builder: (context, ref, _) {
+          final asyncValue = ref.watch(imagesProvider('sea'));
+          return asyncValue.when(
+            error: (error, stackTrace) => const Text('error'),
+            data: (data) => const Text('data'),
+            loading: () => _gridView(),
           );
         },
       ),
+    );
+  }
+
+  Widget _gridView() {
+    return GridView.builder(
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+      itemCount: imageList.length,
+      itemBuilder: (context, index) {
+        final image = imageList[index];
+        final pixabayImage = PixabayImage.fromMap(image);
+        return InkWell(
+          onTap: () async {
+            shareImage(pixabayImage.webformatURL.toString());
+          },
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.network(
+                pixabayImage.previewURL.toString(),
+                fit: BoxFit.cover,
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                    color: Colors.white,
+                    child: Text(' 👍${pixabayImage.likes.toString()} ')),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
--- lib/main_model.dart
+++ lib/main_model.dart
@@ -1,34 +1,68 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
-Future<List> fetchImages(String text) async {
-  const pixabayKey = '32493323-0db8d050baa6e6cc76d89fb4b';
-  const perPage = 100;
+final futureProvider = FutureProvider<dynamic>((ref) async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 'Hello World';
+});
 
-  var options = BaseOptions(
-    baseUrl: 'https://pixabay.com/api',
-    connectTimeout: 5000,
-    receiveTimeout: 3000,
-  );
-  final dio = Dio(options);
+final imagesProvider = FutureProvider.family<WidgetRef, String>(
+  ((ref, text) async {
+    const pixabayKey = '32493323-0db8d050baa6e6cc76d89fb4b';
+    const perPage = 100;
 
-  Response response = await dio.get(
-    '',
-    queryParameters: {
-      'key': pixabayKey,
-      'q': text,
-      'image_type': 'photo',
-      'per_page': perPage,
-    },
-  );
-  print(response.data);
+    var options = BaseOptions(
+      baseUrl: 'https://pixabay.com/api',
+      connectTimeout: 5000,
+      receiveTimeout: 3000,
+    );
+    final dio = Dio(options);
 
-  final imageList = response.data['hits'];
-  return imageList;
-}
+    Response response = await dio.get(
+      '',
+      queryParameters: {
+        'key': pixabayKey,
+        'q': text,
+        'image_type': 'photo',
+        'per_page': perPage,
+      },
+    );
+    print(response.data);
+
+    final imageList = response.data['hits'];
+    return imageList;
+  }),
+);
+
+// Future<List> fetchImages(String text) async {
+//   const pixabayKey = '32493323-0db8d050baa6e6cc76d89fb4b';
+//   const perPage = 100;
+
+//   var options = BaseOptions(
+//     baseUrl: 'https://pixabay.com/api',
+//     connectTimeout: 5000,
+//     receiveTimeout: 3000,
+//   );
+//   final dio = Dio(options);
+
+//   Response response = await dio.get(
+//     '',
+//     queryParameters: {
+//       'key': pixabayKey,
+//       'q': text,
+//       'image_type': 'photo',
+//       'per_page': perPage,
+//     },
+//   );
+//   print(response.data);
+
+//   final imageList = response.data['hits'];
+//   return imageList;
+// }
 
 Future<void> shareImage(String imagePath) async {
   Directory dir = await getTemporaryDirectory();
