fc050d582f9693c18ec1ba10d17b93f02b2e6b17
Daisuke Osanai <dsk.osanai@gmail.com>
2023-01-30 13:18:03 +0900

    fix riverpod version

--- lib/main.dart
+++ lib/main.dart
@@ -26,11 +26,12 @@ class MyApp extends StatelessWidget {
 class PixabayPage extends ConsumerWidget {
   PixabayPage({Key? key}) : super(key: key);
 
-  final textController = TextEditingController();
+  final textController = TextEditingController(text: 'kabuki');
 
   @override
   Widget build(context, ref) {
-    final asyncValue = ref.watch(futureProvider(textController.text));
+    final asyncImage = ref.watch(imageProvider(textController.text));
+    final asyncDate = ref.watch(oneProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -45,97 +46,74 @@ class PixabayPage extends ConsumerWidget {
               controller: textController,
               onFieldSubmitted: (text) async {
                 print('submitt!!!!!!!!!!!!!!!');
-                ref.refresh(futureProvider(text));
-                ref.refresh(futureProvider(textController.text));
               },
             );
           },
         ),
       ),
-      body: Consumer(
-        builder: (context, ref, _) {
-          return asyncValue.when(
+      body: Column(
+        children: [
+          asyncImage.when(
             error: (error, stackTrace) => const Text('error'),
             loading: () => const Text('loading'),
-            data: (data) => _gridView(data),
-          );
-        },
+            data: (data) => Text(data.toString()),
+            // data: (data) => _gridView(data),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.refresh),
         onPressed: () async {
           // 状態を更新する
           print('click!!!!!!!!!!!!!!!!!!!!!!!!');
-          ref.read(futureProvider(textController.text));
-          ref.refresh(futureProvider(textController.text));
+          ref.refresh(oneProvider);
+          // ref.refresh(oneProvider);
+          // ref.invalidate(imageProvider('sushi'));
+          // ref.invalidate;
+          // ref.invalidate(oneProvider);
+
+          // await ref.read(imageProvider('sushi').future);
         },
       ),
     );
   }
 
   Widget _gridView(List data) {
-    return GridView.builder(
+    final images = data.map((e) {
+      final pixabayImage = PixabayImage.fromMap(e);
+      return InkWell(
+        onTap: () async {
+          shareImage(pixabayImage.webformatURL.toString());
+        },
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.network(
+              pixabayImage.previewURL.toString(),
+              fit: BoxFit.cover,
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                  color: Colors.white,
+                  child: Text(' 👍${pixabayImage.likes.toString()} ')),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+
+    return GridView(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
       gridDelegate:
           const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-      itemCount: data.length,
-      itemBuilder: (context, index) {
-        final image = data[index];
-        final pixabayImage = PixabayImage.fromMap(image);
-        return InkWell(
-          onTap: () async {
-            shareImage(pixabayImage.webformatURL.toString());
-          },
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              Image.network(
-                pixabayImage.previewURL.toString(),
-                fit: BoxFit.cover,
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Container(
-                    color: Colors.white,
-                    child: Text(' 👍${pixabayImage.likes.toString()} ')),
-              ),
-            ],
-          ),
-        );
-      },
+      children: images,
     );
   }
-}
 
-// FutureProviderの作成 (単一のデータを非同期で取得する)
-final futureProvider1 = FutureProvider<dynamic>((ref) async {
-  await Future.delayed(const Duration(seconds: 3));
-  return 'Hello World';
-});
-
-// FutureProviderを作成すると「AsyncValue」オブジェクトを生成できる
-//「AsyncValue」は非同期通信の通信中、通信終了、異常終了処理をハンドリングしてくれるRiverpodの便利な機能のこと
-
-class HomePage extends ConsumerWidget {
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // AsyncValueオブジェクトを取得する
-    final asyncValue = ref.watch(futureProvider1);
-    return Scaffold(
-      appBar: AppBar(title: const Text('Hallo World')),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.refresh),
-        onPressed: () {
-          // 状態を更新する
-          ref.refresh(futureProvider1);
-        },
-      ),
-      body: Center(
-        child: asyncValue.when(
-          error: (err, _) => Text(err.toString()), //エラー時
-          loading: () => const CircularProgressIndicator(), //読み込み時
-          data: (data) => Text(data.toString()), //データ受け取り時
-        ),
-      ),
-    );
-  }
+  final dateProvider = FutureProvider<dynamic>((ref) async {
+    // await Future.delayed(const Duration(seconds: 3));
+    return DateTime.now();
+  });
 }
--- lib/main_model.dart
+++ lib/main_model.dart
@@ -5,14 +5,31 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
-final futureProvider = FutureProvider.family((ref, text) async {
+// final dateProvider = FutureProvider<dynamic>((ref) async {
+//   // await Future.delayed(const Duration(seconds: 3));
+//   return DateTime.now();
+// });
+
+final oneProvider = FutureProvider<dynamic>((ref) async {
+  // await Future.delayed(const Duration(seconds: 3));
+  // DateTime.now();
+  return 1.toString();
+});
+
+// final one1Provider = FutureProvider<dynamic>((ref) async {
+//   // await Future.delayed(const Duration(seconds: 3));
+//   // DateTime.now();
+//   return imageProvider;
+// });
+
+final imageProvider = FutureProvider.family((ref, text) async {
   const pixabayKey = '32493323-0db8d050baa6e6cc76d89fb4b';
   const perPage = 100;
-
+  DateTime.now();
   var options = BaseOptions(
     baseUrl: 'https://pixabay.com/api',
-    connectTimeout: 5000,
-    receiveTimeout: 3000,
+    connectTimeout: 50000,
+    receiveTimeout: 30000,
   );
   final dio = Dio(options);
 
@@ -27,10 +44,12 @@ final futureProvider = FutureProvider.family((ref, text) async {
   );
   print(response.data);
 
-  final imageList = response.data['hits'];
+  final imageList = await response.data['hits'];
+  print(await response.data['hits']);
+  await Future.delayed(const Duration(seconds: 3));
+
   return imageList;
 });
-
 
 Future<void> shareImage(String imagePath) async {
   Directory dir = await getTemporaryDirectory();
