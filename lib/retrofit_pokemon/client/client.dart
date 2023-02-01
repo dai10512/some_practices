9e3da5e434d579912a8566efcbd0c807b6e9749c
Daisuke Osanai <dsk.osanai@gmail.com>
2023-02-01 11:57:15 +0900

    fix client

--- lib/retrofit_pokemon/client/client.dart
+++ lib/retrofit_pokemon/client/client.dart
@@ -5,11 +5,9 @@ import '../model/pokeType.dart';
 
 part 'client.g.dart';
 
-
-@RestApi(baseUrl: "https://pokeapi.com/api/v2")
+@RestApi(baseUrl: "https://pokeapi.co/api/v2")
 abstract class RestClient {
   factory RestClient(Dio dio, {String baseUrl}) = _RestClient;
-
   @GET("/type/{type}")
   Future<PokeType> getNamesWithType(@Path("type") String type);
 }
